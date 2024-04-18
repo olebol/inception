e2a8e34b4cb4159d7317ef74f0ba6db9fb5158a2
olebol <ole.pelser@gmail.com>
2024-04-18 20:29:02 +0200

    init shell script readability

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-{
-	# Create database
-	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+# Create database
+echo "CREATE DATABASE IF NOT EXISTS '$DB_NAME';" | mysql
 
-	# Create user
-	echo "CREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD';"
+# Create user
+echo "CREATE USER IF NOT EXISTS '$DB_USERNAME'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
 
-	# Give user required privileges
-	echo "GRANT ALL ON $DB_NAME.* TO $DB_USER@'%';"
+# Give user required privileges
+echo "GRANT ALL ON '$DB_NAME'.* TO '$DB_USERNAME'@'%';" | mysql
 
-	# Reload SQL privileges to update what we did
-	echo "FLUSH PRIVILEGES;"
-} | mysql
+# Reload SQL privileges to update what we did
+echo "FLUSH PRIVILEGES;" | mysql
 
 # Run mariaDB in the foreground
-echo "Running MariaDB"
 exec mysqld
