e0ba92ad9fb1ea839068f079890025317a236015
olebol <ole.pelser@gmail.com>
2024-06-05 21:53:44 +0200

    fix mariadb init script

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -15,8 +15,8 @@ if [ ! -d /run/mysqld ]; then
 	{
 		echo "FLUSH PRIVILEGES;"
 		echo "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;"
-		echo "CREATE USER IF NOT EXISTS $DB_USER@'%' IDENTIFIED BY '$DB_PASS';"
-		echo "GRANT ALL ON \`$DB_NAME\`.* TO $DB_USER@'%';"
+		echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+		echo "GRANT ALL ON \`$DB_NAME\`.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
 		echo "FLUSH PRIVILEGES;"
 	} | mysqld --bootstrap
 
